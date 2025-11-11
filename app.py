61227c35c33b6e9465f26587a8aef36b84307590
joshua-dataprophet <joshua@dataprophet.com>
2025-11-11 08:28:28 +0200

    Added docker-compose file and updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Flask app is running!!"
+    return "Flask app has been updated!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
